8b51832138caeb9a08f06329931eb81f5a69562f
Tobias Henning <tobias081996@googlemail.com>
2024-02-16 12:44:49 +0100

    Changed label map path

--- detector.py
+++ detector.py
@@ -23,7 +23,7 @@ buzzer_pin = 18
 model = tf.saved_model.load('.')
 
 # Load the label map
-label_map_path = 'label_map.pbtxt'
+label_map_path = 'object_detection/data/mscoco_complete_label_map.pbtxt'
 label_map = label_map_util.load_labelmap(label_map_path)
 categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=90, use_display_name=True)
 category_index = label_map_util.create_category_index(categories)
